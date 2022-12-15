4e621585c6fb6fcde08e81ebe159a034d4a584c2
Henrique Grizzo Bernini <henrique_grizzo@yahoo.com.br>
2022-12-15 19:31:01 -0300

    Serviço finalizado e testado

---
+++ src/application/use-cases/count-recipient-notifications.spec.ts
@@ -0,0 +1,23 @@
+import {InMemoryNotificationsRepository} from "@test/repositories/in-memory-notifications-respository"
+import { CountRecipientNotifications } from "./count-recipient-notifications"
+import { makeNotification } from "@test/factory/notification-factory"
+
+
+describe('Count recipient Notification', () => {
+  it('Should be able to count recipient notifications',async() => {
+    const notificationsRepository = new InMemoryNotificationsRepository()
+    const countRecipientNotifications = new CountRecipientNotifications(notificationsRepository)
+
+    await notificationsRepository.create(makeNotification({ recipientId: 'recipient-1'}))
+
+    await notificationsRepository.create(makeNotification({ recipientId: 'recipient-1'}))
+
+    await notificationsRepository.create(makeNotification({ recipientId: 'recipient-2'}))
+
+    const {count} = await countRecipientNotifications.execute({
+      recipientId: 'recipient-1'
+     })
+    
+      expect(count).toEqual(2)
+  })
+})
--- src/application/use-cases/send-notifications.spec.ts
+++ src/application/use-cases/send-notifications.spec.ts
@@ -1,17 +1,18 @@
-import { SendNotification } from "./send-notification"
-import { inMemoryNotificationsRepository } from "test/repositories/in-memory-notifications-respository"
+import { InMemoryNotificationsRepository } from "@test/repositories/in-memory-notifications-respository";
+import {   SendNotification } from "./send-notification";
 
-describe('Send Notification', () => {
-  it('Should be able to send a notification',async() => {
-    const notificationsRepository = new inMemoryNotificationsRepository
-    const sendNotification = new SendNotification(notificationsRepository)
+describe('Send notificaiton',() => {
+  it('should be able to sendo a notificaiton', async() => {
+    const notificationsRepository = new InMemoryNotificationsRepository()
+    const sendNotification = new SendNotification(notificationsRepository);
 
-    await sendNotification.execute({
-      content: 'This is a notification',
+    const { notification } = await sendNotification.execute({
+      content: 'this is a notification',
       category: 'social',
-      recipientId: 'example-recipient-id'
+      recipientId: 'exemple=recipient-id'
     })
-    
+
     expect(notificationsRepository.notifications).toHaveLength(1)
+    expect(notificationsRepository.notifications[0]).toEqual(notification)
   })
-})
+}) 
